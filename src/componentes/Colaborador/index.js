d7633d2619c5b2c5d95d5d5b9ff444adcd486219
flavioborgesnunes <flavioborgesnunes@gmail.com>
2023-03-03 11:44:16 -0300

    organo pronto

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador= ({nome, imagem, cargo, corDeFundo})=>{ //parâmetros desestruturados ao invés de props
+    return(
+        <div className='colaborador'>
+            <div className='cabecalho' style={{backgroundColor:corDeFundo}}>
+                <img src={imagem} alt={nome}/>
+            </div>
+            <div className='rodape'>
+                <h4>{nome}</h4>
+                <h5>{cargo}</h5>
+
+            </div>
+        </div>
+    )
+}
+export default Colaborador
